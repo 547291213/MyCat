54f3af13224f6ba8065151813ceca4889f31e225
kefeng <xp134525@foxmail.com>
2018-12-06 22:23:18 +0800

    1 好友布局的修改提上日程，尚未完成。

---
+++ app/src/main/java/com/example/xkfeng/mycat/DrawableView/SideBar.java
@@ -0,0 +1,124 @@
+package com.example.xkfeng.mycat.DrawableView;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.xkfeng.mycat.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SideBar extends View {
+    private static final String TAG = "SideBar";
+    private Context mContext;
+    public static String[] INDEX_STRING = {"A", "B", "C", "D", "E", "F", "G",
+            "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    private int choose = -1;
+    private List<String> lists;
+    private Paint mPaint;
+    private TextView mTextView;
+    private OnTouchLetterChanged onTouchLetterChanged;
+    private int mWidth;
+    private int mHeight;
+    private int singleLetterHeight;
+    private Paint paint;
+
+    public SideBar(Context context) {
+        this(context, null);
+    }
+
+    public SideBar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+        setBackgroundColor(Color.parseColor("#F0F0F0"));
+        lists = Arrays.asList(INDEX_STRING);
+        paint = new Paint();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        mWidth = getWidth();
+        mHeight = getHeight();
+        singleLetterHeight = mHeight / lists.size();
+        for (int i = 0; i < lists.size(); i++) {
+            paint.setColor(Color.parseColor("#606060"));
+            paint.setTypeface(Typeface.DEFAULT_BOLD);
+            paint.setAntiAlias(true);
+            paint.setTextSize(20);
+            // 选中的状态
+            if (i == choose) {
+                paint.setColor(Color.parseColor("#4F41FD"));
+                paint.setFakeBoldText(true);
+            }
+            //绘制具体的坐标
+            float x = mWidth / 2 - paint.measureText(lists.get(i)) / 2;
+            float y = i * singleLetterHeight + singleLetterHeight / 2;
+            canvas.drawText(lists.get(i), x, y, paint);
+            paint.reset();
+
+        }
+
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        int y = (int) event.getY();
+        int oldChoose = choose;
+        int data = y / singleLetterHeight;
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_UP:
+                setBackgroundColor(Color.parseColor("#F0F0F0"));
+                //choose = -1;
+                invalidate();
+                if (mTextView != null) {
+                    mTextView.setVisibility(GONE);
+                }
+                break;
+
+            default:
+                setBackgroundColor(mContext.getResources().getColor(R.color.lighter_gray));
+                if (oldChoose != data){
+                    if (data >= 0 && data < lists.size()){
+                        choose = data ;
+                        if (onTouchLetterChanged != null){
+                            onTouchLetterChanged.onTouchLetterChanged(lists.get(data));
+                        }
+                        if (mTextView != null){
+                            mTextView.setVisibility(VISIBLE);
+                            mTextView.setText(lists.get(data));
+                        }
+
+                        invalidate();
+                    }
+
+                }
+                    break;
+        }
+        return true;
+    }
+
+
+    public void setmTextView(TextView textView){
+        mTextView = textView ;
+    }
+
+    public void setOnTouchLetterChanged(OnTouchLetterChanged onTouchLetterChanged) {
+        this.onTouchLetterChanged = onTouchLetterChanged;
+    }
+
+    public interface OnTouchLetterChanged {
+        public void onTouchLetterChanged(String s);
+    }
+
+}
---
+++ app/src/main/java/com/example/xkfeng/mycat/Model/FriendInfo.java
@@ -0,0 +1,25 @@
+package com.example.xkfeng.mycat.Model;
+
+import cn.jpush.im.android.api.model.UserInfo;
+
+public class FriendInfo {
+    private UserInfo userInfo ;
+    private String firstLetter ;
+
+
+    public String getFirstLetter() {
+        return firstLetter;
+    }
+
+    public UserInfo getUserInfo() {
+        return userInfo;
+    }
+
+    public void setFirstLetter(String firstLetter) {
+        this.firstLetter = firstLetter;
+    }
+
+    public void setUserInfo(UserInfo userInfo) {
+        this.userInfo = userInfo;
+    }
+}
